22020421fa7d6887ef778991740ad456251015e0
saidkayed <36620432+saidkayed@users.noreply.github.com>
2019-01-30 18:46:33 +0100

    day1unity

---
+++ Assets/Script/User_input.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class User_input : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("f"))
+        {
+            print("key F is pressed");
+        }
+        if (Input.GetKeyDown("w"))
+        {
+            Vector3 position = this.transform.position;
+            position.z++;
+            this.transform.position = position;
+        }
+        if (Input.GetKeyDown("s"))
+        {
+            Vector3 position = this.transform.position;
+            position.z--;
+            this.transform.position = position;
+        }
+        if (Input.GetKeyDown("d"))
+        {
+            Vector3 position = this.transform.position;
+            position.x++;
+            this.transform.position = position;
+        }
+        if (Input.GetKeyDown("a"))
+        {
+            Vector3 position = this.transform.position;
+            position.x--;
+            this.transform.position = position;
+        }
+    }
+
+    
+}
---
+++ Assets/Script/onClick.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class onClick : MonoBehaviour
+{
+    public string _printText;
+    public GameObject TestCube;
+    public int antal;
+    public AudioClip gunshot; //set this in ispector with audiofile
+    public AudioClip kabom; //set this in ispector with audiofile
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void printText()
+    {
+        print(_printText);
+    }
+
+
+    //sådan hiver man fat i komponenter fra unity
+    public void playSound()
+    {
+        GetComponent<AudioSource>().clip = gunshot;
+        GetComponent<AudioSource>().Play();
+    }
+    public void addObject()
+    {
+        for (int i = 0; i < antal; i++) {
+            Instantiate(TestCube, new Vector3(3, 5, 0), Quaternion.identity);
+        }
+    }
+
+    public void boom()
+    {
+
+        GetComponent<AudioSource>().clip = kabom;
+        GetComponent<AudioSource>().Play();
+        for (int i = 0; i < 100; i++)
+        {
+            Instantiate(TestCube, new Vector3(3, 5, 0), Quaternion.identity);
+        }
+    }
+}
