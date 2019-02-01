b48d11b101e71bb440cfa55368672fb3ea472c15
saidkayed <36620432+saidkayed@users.noreply.github.com>
2019-02-01 11:58:13 +0100

    ball movement

--- Assets/Script/User_input.cs
+++ Assets/Script/User_input.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class User_input : MonoBehaviour
 {
+    public float speed;
+
+    private Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
+    /*
     void Update()
     {
+
         if (Input.GetKeyDown("f"))
         {
             print("key F is pressed");
@@ -42,6 +47,16 @@ public class User_input : MonoBehaviour
             this.transform.position = position;
         }
     }
+    */
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
 
-    
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
 }
+    
+
